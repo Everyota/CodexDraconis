736c1f9df6df59285b321a395f1d5b6175b913c0
Sofía Mellado <sofi.mellado@duocuc.cl>
2026-05-06 16:06:19 -0400

    COMMIT

---
+++ src/main/java/com/duoc/Orbitales/OrbitalesApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.Orbitales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitalesApplication.class, args);
+	}
+
+}
