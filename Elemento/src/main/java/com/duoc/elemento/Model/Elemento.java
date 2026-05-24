4b76088fb2384fa5e6ae346abc912096e32d80bb
Sofía Mellado <sofi.mellado@duocuc.cl>
2026-05-24 10:03:39 -0400

    Agregando validaciones a Elemento y Usuario

--- Elemento/src/main/java/com/duoc/elemento/Model/Elemento.java
+++ Elemento/src/main/java/com/duoc/elemento/Model/Elemento.java
@@ -2,6 +2,8 @@ package com.duoc.elemento.Model;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -20,9 +22,12 @@ public class Elemento {
     @Column(name = "id_elemento", nullable = false)
     private Integer id;
 
+    @NotBlank(message = "El nombre del elemento es obligatorio")
+    @Size(max = 80, message = "El nombre del elemento no puede exceder los 80 caracteres")
     @Column(name = "nombre_elemento", nullable = false, length = 80)
     private String nombreElemento;
 
+    @NotBlank(message = "La descripción del elemento es obligatoria")
     @Column(name = "descripcion")
     private String descripcion;
 
--- usuario/src/main/java/com/duoc/usuario/Model/Usuario.java
+++ usuario/src/main/java/com/duoc/usuario/Model/Usuario.java
@@ -1,7 +1,7 @@
 package com.duoc.usuario.Model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,22 +23,22 @@ public class Usuario {
     private Integer id;
 
     @Size(max = 50)
-    @NotNull
+    @NotBlank(message = "El nickname es obligatorio")
     @Column(name = "nickname", nullable = false, length = 50)
     private String nickname;
 
     @Size(max = 80)
-    @NotNull
+    @NotBlank(message = "Los nombres son obligatorios")
     @Column(name = "nombres", nullable = false, length = 80)
     private String nombres;
 
     @Size(max = 80)
-    @NotNull
+    @NotBlank(message = "Los apellidos son obligatorios")
     @Column(name = "apellidos", nullable = false, length = 80)
     private String apellidos;
 
     @Size(max = 120)
-    @NotNull
+    @NotBlank(message = "El correo es obligatorio")
     @Column(name = "correo", nullable = false, length = 120)
     private String correo;
 
@@ -51,19 +51,16 @@ public class Usuario {
     @Column(name = "biografia")
     private String biografia;
 
-    @Size(max = 2)
-    @NotNull
+    @Size(max = 2, message = "El valor de recibeNotificaciones debe ser 'SI' o 'NO'")
     @ColumnDefault("'SI'")
     @Column(name = "recibe_notificaciones", nullable = false, length = 2)
     private String recibeNotificaciones;
 
-    @Size(max = 20)
-    @NotNull
+    @Size(max = 20, message = "El estado del usuario debe tener hasta 20 caracteres")
     @ColumnDefault("'ACTIVO'")
     @Column(name = "estado_usuario", nullable = false, length = 20)
     private String estadoUsuario;
 
-    @NotNull
     @CreationTimestamp
     @Column(name = "fecha_creacion", nullable = false)
     private Instant fechaCreacion;
